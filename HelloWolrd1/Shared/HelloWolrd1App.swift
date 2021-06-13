211d316dbff9db3d418c831e462b9600cadf7d69
Antonella Campos Iannacone <antonellacamposiannacone@Antonellas-MacBook-Pro.local>
2021-06-13 11:51:00 -0400

    added HelloWorld

---
+++ HelloWolrd1/Shared/HelloWolrd1App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWolrd1App.swift
+//  Shared
+//
+//  Created by Antonella Campos Iannacone on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWolrd1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
